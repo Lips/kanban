352c02254d1a75ad81a646bea6eb9247951eb112
LP <111973+Lips@users.noreply.github.com>
2025-01-12 14:40:21 -0500

    Adds polyfill to load-pyj.js

--- js/load-pyj.js
+++ js/load-pyj.js
@@ -1,3 +1,13 @@
+if (!crypto.randomUUID) {
+    crypto.randomUUID = () => {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            const r = Math.random() * 16 | 0;
+            const v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    };
+}
+
 const compiler = RapydScript.create_embedded_compiler();
 window.onload = async () => {
   for (script of document.querySelectorAll("script[type='pyj']")) {
